ef27eabd68ebcc98ed96f15520b711f7ef3dc544
simxnet <simxnet@envs.net>
2024-05-11 13:18:48 +0200

    feat: vanity bot tab

--- src/components/modules/bot/tabs/manage/vanity.tsx
+++ src/components/modules/bot/tabs/manage/vanity.tsx
@@ -1,22 +1,54 @@
-import type { BotObject } from "@/lib/types/apollo";
+import {
+	VanityType,
+	useCreateVanityMutation,
+	useGetVanityQuery,
+	type BotObject,
+} from "@/lib/types/apollo";
+import { handleError } from "@/lib/utils/common";
 import { Button } from "@nextui-org/button";
 import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
 import { Input } from "@nextui-org/input";
+import { useState } from "react";
+import { toast } from "sonner";
 
-export default function ManageVanityBotTab({ id: _id }: Pick<BotObject, "id">) {
+export default function ManageVanityBotTab({ id }: Pick<BotObject, "id">) {
+	const [vanity, setVanity] = useState<string | null>(null);
+	useGetVanityQuery({
+		variables: { input: { id } },
+		onCompleted: (data) => setVanity(data.getVanity.id),
+		errorPolicy: "ignore",
+	});
+
+	const [createVanity, { loading: creatingVanity }] = useCreateVanityMutation({
+		onCompleted: () => toast.success("Vanity updated 👌"),
+		onError: handleError,
+	});
 	return (
 		<div>
 			<Card classNames={{ base: "p-2" }}>
 				<CardHeader className="text-2xl font-bold">Bot vanity</CardHeader>
 				<CardBody>
-					<p>
-						You can set a vanity for your bot, so people can easily find your
-						bot. This vanity will be used in the bot link.
-					</p>
-					<Input placeholder="E.g: elyam" />
+					<Input
+						onChange={(e) => setVanity(e.currentTarget.value)}
+						label="Vanity"
+						labelPlacement="outside"
+						placeholder="Your vanity"
+						description={`E.g: /bot/${id} -> /b/${vanity ?? "akashi"}`}
+					/>
 				</CardBody>
 				<CardFooter>
-					<Button variant="faded" className="w-fit">
+					<Button
+						onClick={() =>
+							createVanity({
+								variables: {
+									input: { targetId: id, id: vanity!, type: VanityType.Bot },
+								},
+							})
+						}
+						isLoading={creatingVanity}
+						variant="faded"
+						className="w-fit"
+					>
 						Update vanity
 					</Button>
 				</CardFooter>
